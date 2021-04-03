1d6589fe2bdf79b85793106b23de01b5b92067ff
Ben <bsth2@kent.ac.uk>
2021-04-03 02:29:01 +0100

    Added 4channel support

--- background.js
+++ background.js
@@ -1,9 +1,14 @@
 chrome.runtime.onInstalled.addListener(function() {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
-            conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: {hostEquals: 'boards.4chan.org'}
-            })],
+            conditions: [
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: {hostEquals: 'boards.4chan.org'}
+                }),
+                new chrome.declarativeContent.PageStateMatcher({
+                    pageUrl: {hostEquals: 'boards.4channel.org'}
+                })
+            ],
             actions: [new chrome.declarativeContent.ShowBrowserAction()]
         }]);
     });
